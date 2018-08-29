86940499f9e3349d9ebe568e9bb29b9505ccfc53
chandana972 <42571801+chandana972@users.noreply.github.com>
2018-08-29 14:47:11 +0530

    Create greater.py

---
+++ greater.py
@@ -0,0 +1,9 @@
+a=float(input("enter the value of a:"))
+b=float(input("enter the value of b:"))
+c=float(input("enter the value of c:"))
+if (a>b) and (a>c):
+  print("a is greater")
+elif (b>a) and (b>c):
+  print("b is greater")
+else:
+  print("c is greater")
